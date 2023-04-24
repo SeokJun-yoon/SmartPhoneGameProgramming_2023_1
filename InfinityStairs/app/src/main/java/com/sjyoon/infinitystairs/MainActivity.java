d9a48368431df20f1dcb614b651be1386ed7a6aa
SeokJun-Yoon <sjyoon0@naver.com>
2023-04-24 19:37:02 +0900

    Test Btn Update
    add start button test

--- InfinityStairs/app/src/main/java/com/sjyoon/infinitystairs/MainActivity.java
+++ InfinityStairs/app/src/main/java/com/sjyoon/infinitystairs/MainActivity.java
@@ -1,8 +1,8 @@
 package com.sjyoon.infinitystairs;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
